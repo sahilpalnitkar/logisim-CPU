45e8ebb6877878782dbcb7e2575525e21849afb4
Unknown <antnguyen60@gmail.com>
2019-04-15 16:13:27 -0500

    Add C++ matrix multiplication implementation

---
+++ cpp/Matrix_Mult.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int mat1[3][3] = 
+    {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    int mat2[3][3] = 
+    {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    int mat3[3][3];
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                mat2[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }  
+
+    return 0;
+}
